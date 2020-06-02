af75cef7a44654c7cd7f6654a20ae2dfb92f205f
Helen Darden <hgdarden@gmail.com>
2020-06-02 18:29:53 -0400

    updating path and get notes api

--- server.js
+++ server.js
@@ -8,23 +8,25 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//html routes====================================
+
+//HTML routes====================================
 
 //return notes.html
 app.get("/notes", function(req, res){
-    res.sendFile(path.join(_dirname, "/notes.html"))
+    res.sendFile(path.join(_dirname, "/public/notes.html"))
 })
 
 //return index.html
 app.get("*", function(req, res){
-    res.sendFile(path.join("index.html"))
+    res.sendFile(path.join(__dirname, "/public/index.html"))
 })
+
 
 //API routes======================================
 
 //reads db.json file and return all saved notes as JSON
 app.get("/api/notes", function(req, res){
-    let savedNotes = "";
+    let savedNotes = fs.readFile(_dirname, "/db/db.json", "utf8");
     return res.json(savedNotes);
 })
 
@@ -32,15 +34,19 @@ app.get("/api/notes", function(req, res){
 //add it to the db.json file
 //and then return the new note to the client.
 app.post("/api/notes", function(req, res){
-
+    let newNote = req.body;
+    
+    fs.writeFile(__dirname, "/db/db.json", (newNote))
 })
 
 //receive a query parameter containing the id of a note to delete,
 //give each note a unique "id" when its saved
 //to delete a note--read all notes from the db.json file, remove the note with the given "id" property, and then rewrite the notes to the db.json file.
 app.delete("api/notes/:id", function(req, res){
+    let noteParamsId = req.params.id;
 
 })
+
 
 //Listener=============================================
 app.listen(PORT, function() {
